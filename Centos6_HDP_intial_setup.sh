29e4da6a6eb5fc70bdc5a6e27b0f6c8d0353c8a2
Pravin Bhagade <iampravinbhagade@gmail.com>
2017-07-28 22:06:34 +0530

    hdp initial
    hdp initial

---
+++ Centos6_HDP_intial_setup.sh
@@ -0,0 +1,36 @@
+
+#setting passwordless ssh for server
+echo "setting passwordless ssh for server"
+
+ssh-keyscan -H localhost > ~/.ssh/known_hosts  
+y|ssh-keygen -t dsa -P '' -f ~/.ssh/id_dsa  
+cat ~/.ssh/id_dsa.pub > ~/.ssh/authorized_keys  
+
+
+yum install -y ntp wget
+chkconfig --list ntpd
+chkconfig ntpd on
+service ntpd start
+chkconfig iptables off
+/etc/init.d/iptables stop
+setenforce 0
+sed -i 's/enforcing/disabled/g' /etc/selinux/config /etc/selinux/config
+sestatus
+umask 0022
+echo umask 0022 >> /etc/profile
+echo "vm.swappiness=1" >> /etc/sysctl.conf 
+echo "1" > /proc/sys/vm/swappiness 
+
+echo never > /sys/kernel/mm/redhat_transparent_hugepage/enabled
+echo never > /sys/kernel/mm/redhat_transparent_hugepage/defrag
+
+
+echo "Ambari repo:
+echo "https://docs.hortonworks.com/HDPDocuments/Ambari-2.5.1.0/bk_ambari-installation/content/ambari_repositories.html"
+echo
+echo "HDP repo"
+echo "https://docs.hortonworks.com/HDPDocuments/Ambari-2.5.1.0/bk_ambari-installation/content/hdp_stack_repositories.html"
+
+echo "amabri upgrade"
+echo "wget -nv http://public-repo-1.hortonworks.com/ambari/centos6/2.x/updates/2.5.1.0/ambari.repo -O /etc/yum.repos.d/ambari.repo"
+
